9ec6f495cfe18af79c585fe4d7956e1b3a5f114d
Nathan Burgo <nthnbrg185@gmail.com>
2018-03-19 14:19:08 -0600

    can now read file and split by \n'

--- my-files.js
+++ my-files.js
@@ -2,15 +2,25 @@ const chalk = require('chalk');
 const path = require('path');
 const fs = require('fs');
 
-// const filename = path.join(__dirname, 'test.txt');
+const filename = path.join(__dirname, 'text.txt');
 // console.log(chalk.red(filename));
 
-fs.readdir(__dirname, (err, files) => {
-  if (err) {
-    console.log(chalk.red(err))
-  } else {
-    console.log(files);
-  }
-})
 
-console.log(chalk.yellow('Hello!'))
+// fs.readdir(__dirname, (err, files) => {
+//   if (err) {
+//     console.log(chalk.red(err))
+//   } else {
+//     console.log(files);
+//   }
+// })
+//
+// console.log(chalk.yellow('Hello!'))
+
+fs.readFile(filename, 'utf8', (err, data) => {
+  if (err) {
+    return console.log(err);
+  }
+  const lines = data.split('\n').filter(x => x.length > 0);
+
+  console.log(lines)
+})
