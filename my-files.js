5d621a654bed957bfd9382dcd0b67448f8a51220
Nathan Burgo <nthnbrg185@gmail.com>
2018-03-19 14:03:29 -0600

    read the directory

--- my-files.js
+++ my-files.js
@@ -1,6 +1,16 @@
 const chalk = require('chalk');
 const path = require('path');
+const fs = require('fs');
 
-const filename = path.join(__dirname, 'test.txt');
+// const filename = path.join(__dirname, 'test.txt');
+// console.log(chalk.red(filename));
 
-console.log(chalk.red(filename));
+fs.readdir(__dirname, (err, files) => {
+  if (err) {
+    console.log(chalk.red(err))
+  } else {
+    console.log(files);
+  }
+})
+
+console.log(chalk.yellow('Hello!'))
